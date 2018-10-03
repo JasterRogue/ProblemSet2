bac6d6cc414c786597eaa09e10b6110754649a9d
Ryan Madigan <t00200799@365s.ittralee.ie>
2018-10-03 15:31:42 +0100

    exercise 6 of problem set 2 is finished Signed-off-by: Ryan Madigan <t00200799@365s.ittralee.ie>

--- src/Fraction.java
+++ src/Fraction.java
@@ -4,6 +4,7 @@ public class Fraction {
     private int denominator;
     private int totalFraction1=0, totalFraction2=0;
     private String op="";
+    private int nA, dA;
 
     public void setNumerator(int numerator)
     {
@@ -42,6 +43,65 @@ public class Fraction {
     public String toString()
     {
         return "";
+    }
+
+    public void addFractions(int numerator, int denominator, int n2, int d2)
+    {
+        int lowestCommonDenominator;
+
+        lowestCommonDenominator = denominator * d2;
+
+        nA = numerator + n2;
+
+        System.out.print(numerator+"/"+denominator + " + " + n2+"/"+d2 + " is " + nA + "/" + lowestCommonDenominator);
+
+
+    }
+
+    public void subtractFractions(int numerator, int denominator, int n2, int d2)
+    {
+        int lowestCommonDenominator;
+
+        lowestCommonDenominator = denominator * d2;
+
+        nA = numerator - n2;
+
+        System.out.print(numerator+"/"+denominator + " + " + n2+"/"+d2 + " is " + nA + "/" + lowestCommonDenominator);
+
+
+    }
+
+    public void multiplyFractions(int numerator, int denominator, int n2, int d2)
+    {
+       // int lowestCommonDenominator;
+
+       // lowestCommonDenominator = denominator * d2;
+
+        nA = numerator * n2;
+        dA = denominator * d2;
+
+        System.out.print(numerator+"/"+denominator + " + " + n2+"/"+d2 + " is " + nA + "/" + d2);
+
+
+    }
+
+    public void divideFractions(int numerator, int denominator, int n2, int d2)
+    {
+        // int lowestCommonDenominator;
+
+        // lowestCommonDenominator = denominator * d2;
+
+        int holder;
+        holder = d2;
+        d2 = n2;
+        n2 = holder;
+
+        nA = numerator * n2;
+        dA = denominator * d2;
+
+        System.out.print(numerator+"/"+denominator + " + " + n2+"/"+d2 + " is " + nA + "/" + d2);
+
+
     }
 
 }
--- src/FractionTest.java
+++ src/FractionTest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class FractionTest {
     public static void main(String[] args)
     {
-        int numerator, denominator;
+        int numerator, denominator, d2, n2, answerNumerator, answerDenominator;
 
         Scanner input = new Scanner(System.in);
         Fraction fraction = new Fraction();
@@ -16,20 +16,36 @@ public class FractionTest {
         denominator = input.nextInt();
         fraction.setDenominator(denominator);
 
+        System.out.print("Please enter the numerator of the second fraction");
+        n2 = input.nextInt();
+
+        System.out.print("Please enter the denominator of the second fraction");
+        d2 = input.nextInt();
+
         System.out.print("1. Add" +"\n2. Subtract" + "\n3. Multiply" + "\n4. Division");
         int userInput = input.nextInt();
 
         if(userInput==1)
         {
-            fraction.addFractions();
+            fraction.addFractions(numerator, denominator, n2, d2);
         }
 
         if(userInput==2)
         {
-
+            fraction.subtractFractions(numerator, denominator, n2, d2);
         }
 
-        System.out.print(fraction.toString());
+        if(userInput==3)
+        {
+            fraction.multiplyFractions(numerator, denominator, n2, d2);
+        }
+
+        if(userInput==4)
+        {
+            fraction.divideFractions(numerator, denominator, n2, d2);
+        }
+
+        //System.out.print(fraction.toString());
 
 
 
